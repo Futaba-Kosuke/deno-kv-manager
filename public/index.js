167a7de8ddae896ce2aa5abd816eb0264fbf98cb
Futaba-Kosuke <k.futaba2828@gmail.com>
2024-07-31 15:57:06 +0900

    Add: データの挿入処理を実装

--- public/index.js
+++ public/index.js
@@ -14,10 +14,28 @@ const getAll = async () => {
   $("#kv-rows").empty();
   // 要素内にデータ挿入
   for (const row of rows) {
+    const key = row["key"];
+    const value = row["value"];
+    const type = typeof (row["value"]);
+
     $("#kv-rows").append(`
       <tr>
-        <td>${row["key"]}</td>
-        <td>${row["value"]}</td>
+        <td><input class="is-target" type="checkbox"></td>
+        <td>${JSON.stringify(key)}</td>
+        <td>${type === "object" ? JSON.stringify(value) : value}</td>
+        <td>
+          <select>
+            <option value="json" ${
+      type === "object" ? "selected" : ""
+    }>json</option>
+            <option value="string" ${
+      type === "string" ? "selected" : ""
+    }>string</option>
+            <option value="number" ${
+      type === "number" ? "selected" : ""
+    }>number</option>
+          </select>
+        </td>
       </tr>
     `);
   }
@@ -25,20 +43,69 @@ const getAll = async () => {
   return;
 };
 
+/**
+ * 新規行のHTML要素を追加
+ */
+const addRowElement = () => {
+  const rows = $("#kv-rows");
+
+  const rowCount = rows.children().length;
+  rows.append(`
+    <tr class="new-kv-row">
+      <td><input class="is-target" type="checkbox" checked></td>
+      <td><input class="new-kv-key" type="text" value="[]"></td>
+      <td><input class="new-kv-value type="text"></td>
+      <td>
+        <select class="new-kv-value-type">
+          <option value="json">json</option>
+          <option value="string">string</option>
+          <option value="number">number</option>
+        </select>
+      </td>
+    </tr>
+  `);
+};
+
+/**
+ * データの新規作成・及び最新データの取得
+ * @returns なし
+ */
 const insertRows = async () => {
   const url = getUrl();
   const token = getToken();
-  const rows = [
-    { "key": ["X", "B"], "value": Math.random() },
-    { "key": ["Y", "B"], "value": Math.random() },
-    { "key": ["Z", "B"], "value": Math.random() },
-  ];
+  const rows = [];
 
-  const response = await fetchServer("POST", "/rows", { url, token, rows });
+  $("#kv-rows .new-kv-row").each((index, element) => {
+    const isTarget = $(element).find(".is-target").is(":checked");
 
-  console.log(await response.text());
+    if (isTarget) {
+      const key = JSON.parse($(element).find(".new-kv-key").val());
+      let value;
+      switch ($(element).find(".new-kv-value-type").val()) {
+        case "string":
+          value = $(element).find(".new-kv-value").val();
+          break;
+        case "number":
+          value = Number($(element).find(".new-kv-value").val());
+          break;
+        case "json":
+          value = JSON.parse($(element).find(".new-kv-value").val());
+          break;
+      }
+      rows.push({ key, value });
+    }
+  });
 
-  return response;
+  // データを挿入
+  await fetchServer("POST", "/rows", { url, token, rows });
+
+  // 1秒待つ
+  sleep(1);
+
+  // 最新データを取得
+  await getAll();
+
+  return;
 };
 
 const updateRows = async () => {
