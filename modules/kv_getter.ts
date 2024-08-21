afe353d18f7878cfb4a129f12c61fb20bee5776d
Futaba-Kosuke <k.futaba2828@gmail.com>
2024-08-21 18:39:06 +0900

    Fix: re set previous access token

--- modules/kv_getter.ts
+++ modules/kv_getter.ts
@@ -4,14 +4,20 @@ export const accessKv = async (
   url: string,
   token: string,
 ): Promise<Deno.Kv> => {
+  const prevToken = await Deno.env.get(DENO_KV_ACCESS_TOKEN_KEY);
+
   // リクエストに応じて環境変数を設定する
-  Deno.env.set(DENO_KV_ACCESS_TOKEN_KEY, token);
+  await Deno.env.set(DENO_KV_ACCESS_TOKEN_KEY, token);
 
   // Deno KVの読み込み
   const kv = await Deno.openKv(url);
 
   // 環境変数を削除
-  Deno.env.delete(DENO_KV_ACCESS_TOKEN_KEY);
+  await Deno.env.delete(DENO_KV_ACCESS_TOKEN_KEY);
+
+  if (prevToken !== undefined) {
+    await Deno.env.set(DENO_KV_ACCESS_TOKEN_KEY, prevToken);
+  }
 
   return kv;
 };
